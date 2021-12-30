9f24516a3ec50af88fe7d86419aaa271ef895d1d
Alpha Niang <alpha.niang@hotmail.com>
2021-12-30 14:21:25 +0100

    cours 34

---
+++ src/app/app-routing.module.ts
@@ -0,0 +1,17 @@
+import { NgModule } from '@angular/core';
+import { RouterModule } from '@angular/router';
+
+import { HomeComponent } from './home/home.component';
+
+@NgModule({
+  declarations: [],
+  imports: [
+    RouterModule.forRoot([
+      { path: 'home', component: HomeComponent },
+      { path: '', redirectTo: 'home', pathMatch: 'full' },
+      { path: '**', redirectTo: 'home', pathMatch: 'full' },
+    ]),
+  ],
+  exports: [RouterModule],
+})
+export class AppRoutingModule {}
