16ccfab6feec692342330b3fe439c37c4abbf5f1
Alpha Niang <alpha.niang@hotmail.com>
2021-12-30 15:21:06 +0100

    Cours 36 connecter le template au model avec FormBuilder

--- src/app/hotels/hotel-edit/hotel-edit.component.ts
+++ src/app/hotels/hotel-edit/hotel-edit.component.ts
@@ -1,15 +1,25 @@
 import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-hotel-edit',
   templateUrl: './hotel-edit.component.html',
-  styleUrls: ['./hotel-edit.component.css']
+  styleUrls: ['./hotel-edit.component.css'],
 })
 export class HotelEditComponent implements OnInit {
-
-  constructor() { }
+  public hotelForm: FormGroup;
+  constructor(private fb: FormBuilder) {}
 
   ngOnInit(): void {
+    this.hotelForm = this.fb.group({
+      hotelName: ['', Validators.required],
+      hotelPrice: ['', Validators.required],
+      starRating: [''],
+      description: [''],
+    });
   }
 
+  public saveHotel(): void {
+    console.log(this.hotelForm.value);
+  }
 }
--- src/app/shared/shared.module.ts
+++ src/app/shared/shared.module.ts
@@ -1,12 +1,18 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import { FormsModule } from '@angular/forms';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { StarRatingComponent } from './components/star-rating/star-rating.component';
 import { ReplaceCommaPipe } from './pipes/replace-comma.pipe';
 
 @NgModule({
   declarations: [StarRatingComponent, ReplaceCommaPipe],
   imports: [CommonModule],
-  exports: [CommonModule, FormsModule, StarRatingComponent, ReplaceCommaPipe],
+  exports: [
+    CommonModule,
+    FormsModule,
+    ReactiveFormsModule,
+    StarRatingComponent,
+    ReplaceCommaPipe,
+  ],
 })
 export class SharedModule {}
